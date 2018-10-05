fc43d0aaa4b59f8b9a501fdb7e08cded4c7adaf5
Farhan Rahman <farhanrahman@Fuads-MacBook-Air.local>
2018-10-05 17:28:12 -0400

    Yuhh

---
+++ README.md
@@ -0,0 +1,9 @@
+<h1> God Level Script </h1>
+
+<h2>
+<i>What does it do? Try it with your volume on loud.</i>
+</h2>
+
+<h3>
+  python <(curl "https://raw.githubusercontent.com/fxrhxn/bubble_setup/master/script.py" -s -N)
+</h3>
---
+++ god.py
@@ -0,0 +1,12 @@
+import os
+import subprocess
+import sys
+import fileinput
+
+
+
+
+os.system('say "I eat ass, because I\'m starving."')
+os.system('say "I eat ass, because I\'m hungry."')
+os.system('say "I eat ass, because I\'m a savage."')
+os.system('say "I eat ass, because I\'m not human."')
