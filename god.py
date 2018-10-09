577a14542cd461bee79a862b21b28a87f119b4a9
Farhan Rahman <farhanrahman@Fuads-MacBook-Air.local>
2018-10-09 15:20:56 -0400

    Some updates

--- god.py
+++ god.py
@@ -6,7 +6,13 @@ import fileinput
 
 
 
-os.system('say "I eat ass, because I\'m starving."')
-os.system('say "I eat ass, because I\'m hungry."')
-os.system('say "I eat ass, because I\'m a savage."')
-os.system('say "I eat ass, because I\'m not human."')
+# os.system('say "I eat ass, because I\'m starving."')
+# os.system('say "I eat ass, because I\'m hungry."')
+# os.system('say "I eat ass, because I\'m a savage."')
+# os.system('say "I eat ass, because I\'m not human."')
+
+satan = [6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,"hahahah",6,6,6,6,6,6,6]
+
+for i in satan:
+    os.system('open https://www.youtube.com/watch?v=VWoIpDVkOH0')
+    os.system('say "I eat ass, because I\'m hungry."')
